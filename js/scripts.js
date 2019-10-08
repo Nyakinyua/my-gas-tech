c8e0d650ec0903794e0e9cfbf844ad7bc7363e94
Nyakinyua <wanyakinyua968@gmail.com>
2019-10-08 08:45:20 +0300

    add user login form

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+   $(".btn-login").submit(function(e) {
+        e.preventDefault();
+        var email=$("#loginFormEmail").val();
+        var password=$("#loginFormPassword").val();
+
+        if (email === "") {
+            alert("please input email")
+        }
+        else if(password === "" && password.length < 8){
+            alert("password must be atleast 8 characters")
+        }
+    });
+});
