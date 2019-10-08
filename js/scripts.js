c2f0dff7e4c58441b38fc19a87b9c4e626a7e29e
georgewanyama <wanyamageorge680@gmail.com>
2019-10-08 09:21:35 +0300

    add js file in scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+function GEEKFORGEEKS()                                    
+{ 
+    var name = document.forms["RegForm"]["Name"];               
+    var email = document.forms["RegForm"]["EMail"];    
+    var phone = document.forms["RegForm"]["Telephone"];  
+    var what =  document.forms["RegForm"]["Subject"];  
+    var password = document.forms["RegForm"]["Password"];  
+    var address = document.forms["RegForm"]["Address"];  
+   
+    if (name.value == "")                                  
+    { 
+        window.alert("Please enter your name."); 
+        name.focus(); 
+        return false; 
+    } 
+   
+    if (address.value == "")                               
+    { 
+        window.alert("Please enter your address."); 
+        name.focus(); 
+        return false; 
+    } 
