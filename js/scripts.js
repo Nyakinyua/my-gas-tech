4b9d2cfaaca8e83ab5f70aa7701864bffa2dda13
georgewanyama <wanyamageorge680@gmail.com>
2019-10-08 10:51:10 +0300

    add a link in script.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-function GEEKFORGEEKS()                                    
+function gas()                                    
 { 
     var name = document.forms["RegForm"]["Name"];               
     var email = document.forms["RegForm"]["EMail"];    
@@ -9,55 +9,56 @@ function GEEKFORGEEKS()
    
     if (name.value == "")                                  
     { 
-        window.alert("Please enter your name."); 
+        alert("Please enter your name."); 
         name.focus(); 
         return false; 
     } 
    
     if (address.value == "")                               
     { 
-        window.alert("Please enter your address."); 
+        alert("Please enter your address."); 
         name.focus(); 
         return false; 
     }
-    if (address.value == "")                               
+    if (address.value == "")  
+                                 
     { 
-        window.alert("Please enter your address."); 
+        alert("Please enter your address."); 
         name.focus(); 
         return false; 
     } 
        
     if (email.value == "")                                   
     { 
-        window.alert("Please enter a valid e-mail address."); 
+        alert("Please enter a valid e-mail address."); 
         email.focus(); 
         return false; 
     } 
    
     if (email.value.indexOf("@", 0) < 0)                 
     { 
-        window.alert("Please enter a valid e-mail address."); 
+        alert("Please enter a valid e-mail address."); 
         email.focus(); 
         return false; 
     } 
    
     if (email.value.indexOf(".", 0) < 0)                 
     { 
-        window.alert("Please enter a valid e-mail address."); 
+        alert("Please enter a valid e-mail address."); 
         email.focus(); 
         return false; 
     } 
     
     if (phone.value == "")                           
     { 
-        window.alert("Please enter your telephone number."); 
+        alert("Please enter your telephone number."); 
         phone.focus(); 
         return false; 
     } 
    
     if (password.value == "")                        
     { 
-        window.alert("Please enter your password"); 
+        alert("Please enter your password"); 
         password.focus(); 
         return false; 
     } 
