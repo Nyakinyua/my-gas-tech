5dcf69bfd75d84dfda469bc379760dafac4e4392
Nyakinyua <wanyakinyua968@gmail.com>
2019-10-09 09:51:34 +0300

    add delvery button to allow user to deliver

--- js/scripts.js
+++ js/scripts.js
@@ -1,173 +1,103 @@
-$(document).ready (function(){
-    $(".btn-login").submit(function(e) {
-        e.preventDefault();
-        var email=$("#loginFormEmail").val();
-        var password=$("#loginFormPassword").val();
+//make gas order
+// function updateTotal($form)
+// {  
+//     var total = 0;
 
-        if (email === "") {
-            alert("please input email")
-        }
-        else if(password === "" && password.length < 8){
-            alert("password must be atleast 8 characters")
-        }
-        else{
-            alert("login successful")
-        }
+//     var selectedGasSize = $form.find('select[name="gassize"]:selected').data('price');
+//     if (!isNaN(selectedGasSize))
+//     {
+//         total += selectedGasSize;
+//     }
+//     var action = $form.find('input[name="gas"] option:checked').data('price');
+//     if (!isNaN(action))
+//     {
+//         total += action;
+//     }
+//     var gasType1 = $form.find('input[name="gas-type"] option:checked').data('price');
+//     if (!isNaN(gasType1))
+//     {
+//         total += gasType1;
+//     }
+    
+    
+//     $form.find('input[name="total"]').val(total);
+// }
+// //delivery option
+// var checkVal=$()
+
+
+// $(function() {
+//     $('#gasform1 select[name="gassize"]').change(function () {
+//         var $form = $(this).closest('form');
+//         updateTotal($form);
+//     });
+
+//     $('#gasform1 input[name="gas"]').change(function () {
+//         var $form = $(this).closest('form');
+//         updateTotal($form);
+//     });
+//     $('#gasform1 input[name="gas-type"]').change(function () {
+//         var $form = $(this).closest('form');
+//         updateTotal($form);
+//     });
+// });
+
+function updateTotal($form)
+{  
+    var total = 0;
+
+    var selectedgas = $form.find('input[name="selectedgas"]:checked').data('price');
+    if (!isNaN(selectedgas))
+    {
+        total += selectedgas;
+    }
+
+    var selectedaction=$form.find('input[name="selectedoption"]:checked').data('price');
+    if (!isNaN(selectedaction))
+    {
+        total += selectedaction;
+    }
+    
+    var filling = $form.find('select[name="filling"] option:selected').data('price');
+    if (!isNaN(filling))
+    {
+        total += filling;
+    }
+    
+    $form.find('input[name="total"]').val(total);
+}
+
+$(function() {
+    $('#gasform1 input[name="selectedgas"]').change(function () {
+        var $form = $(this).closest('form');
+        updateTotal($form);
+    }); 
+
+    $('#gasform1 input[name="selectedoption"]').change(function (){
+        var $form= $(this).closest('form');
+        updateTotal($form);
     });
 
-    
-    var location_price=new Array();
-    location_price["none"]=0;
-    locaton_price["Mathare"]=50;
-    location_price["Starehe"]=30;
-    location_price["Kamukunji"]=20;
-    location_price["makadara"]=40;
-    location_price["Embakasi West"]=70;
-    location_price["Embakasi East"]=30;
-    location_price["Embakasi North"]=40;
-    location_Price["embakasi south"]=80;
-    location_price["Ruaraka"]=50;
-    location_price["Kasarani"]=30;
-    location_price["Roysambu"]=40;
-    location_price["Kibra"]=20;
-    location_price["lang'ata"]=60;
-    location_price["Dagoretti North"]=90;
-    location_Price["Dagoretti+=South"]=40;
-    location_price["westlands"]=80;
+    $('#gasform1 select[name="filling"]').change(function () {
+        var $form = $(this).closest('form');
+        updateTotal($form);
+    });
+});
 
 
 
-//}
- /*Set up an associative array
- //The keys represent the size of the cake
- //The values represent the cost of the cake i.e A 10" cake cost's $35
- var cake_prices = new Array();
- cake_prices["Round6"]=20;
- cake_prices["Round8"]=25;
- cake_prices["Round10"]=35;
- cake_prices["Round12"]=75;
- 
- //Set up an associative array 
- //The keys represent the filling type
- //The value represents the cost of the filling i.e. Lemon filling is $5,Dobash filling is $9
- //We use this this array when the user selects a filling from the form
- var filling_prices= new Array();
- filling_prices["None"]=0;
- filling_prices["Lemon"]=5;
- filling_prices["Custard"]=5;
- filling_prices["Fudge"]=7;
- filling_prices["Mocha"]=8;
- filling_prices["Raspberry"]=10;
- filling_prices["Pineapple"]=5;
- filling_prices["Dobash"]=9;
- filling_prices["Mint"]=5;
- filling_prices["Cherry"]=5;
- filling_prices["Apricot"]=8;
- filling_prices["Buttercream"]=7;
- filling_prices["Chocolate Mousse"]=12;
- 
-	 
-	 
-// getCakeSizePrice() finds the price based on the size of the cake.
-// Here, we need to take user's the selection from radio button selection
-function getCakeSizePrice()
-{  
-    var cakeSizePrice=0;
-    //Get a reference to the form id="cakeform"
-    var theForm = document.forms["cakeform"];
-    //Get a reference to the cake the user Chooses name=selectedCake":
-    var selectedCake = theForm.elements["selectedcake"];
-    //Here since there are 4 radio buttons selectedCake.length = 4
-    //We loop through each radio buttons
-    for(var i = 0; i < selectedCake.length; i++)
-    {
-        //if the radio button is checked
-        if(selectedCake[i].checked)
-        {
-            //we set cakeSizePrice to the value of the selected radio button
-            //i.e. if the user choose the 8" cake we set it to 25
-            //by using the cake_prices array
-            //We get the selected Items value
-            //For example cake_prices["Round8".value]"
-            cakeSizePrice = cake_prices[selectedCake[i].value];
-            //If we get a match then we break out of this loop
-            //No reason to continue if we get a match
-            break;
-        }
-    }
-    //We return the cakeSizePrice
-    return cakeSizePrice;
-}
 
-//This function finds the filling price based on the 
-//drop down selection
-function getFillingPrice()
-{
-    var cakeFillingPrice=0;
-    //Get a reference to the form id="cakeform"
-    var theForm = document.forms["cakeform"];
-    //Get a reference to the select id="filling"
-     var selectedFilling = theForm.elements["filling"];
-     
-    //set cakeFilling Price equal to value user chose
-    //For example filling_prices["Lemon".value] would be equal to 5
-    cakeFillingPrice = filling_prices[selectedFilling.value];
 
-    //finally we return cakeFillingPrice
-    return cakeFillingPrice;
-}
 
-//candlesPrice() finds the candles price based on a check box selection
-function candlesPrice()
-{
-    var candlePrice=0;
-    //Get a reference to the form id="cakeform"
-    var theForm = document.forms["cakeform"];
-    //Get a reference to the checkbox id="includecandles"
-    var includeCandles = theForm.elements["includecandles"];
 
-    //If they checked the box set candlePrice to 5
-    if(includeCandles.checked==true)
-    {
-        candlePrice=5;
-    }
-    //finally we return the candlePrice
-    return candlePrice;
-}
 
-function insciptionPrice()
-{
-    //This local variable will be used to decide whether or not to charge for the inscription
-    //If the user checked the box this value will be 20
-    //otherwise it will remain at 0
-    var inscriptionPrice=0;
-    //Get a refernce to the form id="cakeform"
-    var theForm = document.forms["cakeform"];
-    //Get a reference to the checkbox id="includeinscription"
-    var includeInscription = theForm.elements["includeinscription"];
-    //If they checked the box set inscriptionPrice to 20
-    if(includeInscription.checked==true){
-        inscriptionPrice=20;
-    }
-    //finally we return the inscriptionPrice
-    return inscriptionPrice;
-}
-        
-function calculateTotal()
-{
-    //Here we get the total price by calling our function
-    //Each function returns a number so by calling them we add the values they return together
-    var cakePrice = getCakeSizePrice() + getFillingPrice() + candlesPrice() + insciptionPrice();
-    
-    //display the result
-    var divobj = document.getElementById('totalPrice');
-    divobj.style.display='block';
-    divobj.innerHTML = "Total Price For the Cake $"+cakePrice;
 
-}
 
-function hideTotal()
-{
-    var divobj = document.getElementById('totalPrice');
-    divobj.style.display='none';
-}*/
+
+
+
+
+
+
+
+
